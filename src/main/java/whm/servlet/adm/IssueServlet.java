cb5804f650cb3163eef655650be505827046834e
LuongCongTruong2k7 <luongcongtruong2007@gmail.com>
2026-08-02 10:57:13 +0700

    Kiem tra so luong xuat > 0 truoc khi luu phieu xuat

--- src/main/java/whm/servlet/adm/IssueServlet.java
+++ src/main/java/whm/servlet/adm/IssueServlet.java
@@ -1,5 +1,8 @@
 package whm.servlet.adm;
 
 public class IssueServlet {
-
+    // trước khi lưu phiếu xuất
+    if (quantity <= 0) {
+        throw new IllegalArgumentException("So luong xuat phai lon hon 0");
+    }
 }
