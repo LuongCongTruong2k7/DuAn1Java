fb0be6d1a0dd7f0f3628b2f7bb43898b54eded35
LuongCongTruong2k7 <luongcongtruong2007@gmail.com>
2026-08-02 11:04:45 +0700

    print

--- src/main/java/whm/servlet/adm/IssueServlet.java
+++ src/main/java/whm/servlet/adm/IssueServlet.java
@@ -1,8 +1,5 @@
 package whm.servlet.adm;
 
 public class IssueServlet {
-    // trước khi lưu phiếu xuất
-    if (quantity <= 0) {
-        throw new IllegalArgumentException("So luong xuat phai lon hon 0");
-    }
+    System.out.println("Kiem tra phieu xuat");
 }
